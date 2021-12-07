6a9b102482984082a4e5463a31a43f7591f05a68
plarrat <larrat.philippe@lessonsharing.fr>
2021-12-07 17:25:32 +0100

    Tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export function sum(a,b){
+  return a + b ;
+}
+
+export async function getUsers(){
+  let datas = await axios.get("https://jsonplaceholder.typicode.com/users");
+  return datas.data;
+}
 
 function App() {
+  const[users, setUsers] = useState([]);
+  
+  useEffect(()=>{
+    
+  }, [])
+
+  async function getUsers2(){
+    let datas = await axios.get("https://jsonplaceholder.typicode.com/users");
+    setUsers(datas.data);
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mon application de test</h1>
+        <hr />
+
+        <button onClick={getUsers2}>Cliquez moi</button>
       </header>
+      <main>
+        <table>
+          <tbody>
+          {
+            users.map(user=>{
+              return (
+                <tr key={user.id}>
+                  <td>{user.name}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                </tr>
+              )
+            })
+          }
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,57 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import App, {sum, getUsers} from "./App"
+import axios from "axios"
 
-test('renders learn react link', () => {
+jest.mock("axios");
+
+beforeEach(() => axios.mockClear())
+
+it("should add 2 numbers",()=>{
+  let res = sum(2,10);
+  expect(res).toBe(12);
+})
+
+describe("Test of getUsers()", ()=>{
+  it("should return array value", async ()=>{
+    const valueMock = {
+      data:[],
+      status:200
+    };
+
+    axios.get.mockResolvedValue(valueMock);
+    let res = await getUsers();
+    expect(res).toEqual([]);
+  })
+})
+
+it("should render App",()=>{
+  render(<App />)
+})
+
+it("should have button", ()=>{
+  render(<App />)
+  const bouton = screen.getByText("Cliquez moi");
+  expect(bouton).toBeInTheDocument();
+})
+
+it("should display table rows", async ()=>{
+  const valueMock = {
+      data:[
+            {
+              id: 1,
+              name: "Leanne Graham",
+              username: "Bret",
+              email: "Sincere@april.biz"
+            }
+          ]
+  };
+  axios.get.mockResolvedValue(valueMock);
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const bouton = screen.getByText("Cliquez moi");
+  fireEvent.click(bouton);
+  await waitFor(() => screen.getByRole("table"))
+  screen.debug();
+  const rows = screen.queryAllByRole("row");
+  expect(rows).toHaveLength(1);
+})
